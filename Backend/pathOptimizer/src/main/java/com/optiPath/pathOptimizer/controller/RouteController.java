f3b7b76627126848f91ed4edd891310f9c947b1a
Shantanu675 <shantanudanej05@gmail.com>
2025-10-25 12:42:59 +0530

    Update CrossOrigin annotation in RouteController to allow all origins

--- Backend/pathOptimizer/src/main/java/com/optiPath/pathOptimizer/controller/RouteController.java
+++ Backend/pathOptimizer/src/main/java/com/optiPath/pathOptimizer/controller/RouteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://optipath-shantanu.netlify.app/")
+@CrossOrigin()
 @RestController
 @RequestMapping("/")
 public class RouteController {
